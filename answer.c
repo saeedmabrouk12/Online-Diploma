22c7464cab908f304444e1883807c8f4b3023ad3
saeedmabrouk21 <saeedmabrouk21@gmail.com>
2022-08-28 18:41:13 +0200

    Mcu Fundamentals Clock Lesson

---
+++ answer.c
@@ -0,0 +1,10 @@
+//////EX2///////
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter Number : ");
+    // scanf("%d", &number);
+    // printf("You Entered : %d", number);
+    return 0;
+}
